aeb13617f85a0bd01f2dd8a20b617af3b385f024
Nihal Ismail <nihalismailk@gmail.com>
2016-03-05 17:24:50 +0530

    Added splash screen

--- app/src/main/java/com/lmntrx/android/classroom/MainActivity.java
+++ app/src/main/java/com/lmntrx/android/classroom/MainActivity.java
@@ -1,13 +1,25 @@
 package com.lmntrx.android.classroom;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private final int splashDuration=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent launcher=new Intent(getApplicationContext(),Classroom.class);
+                startActivity(launcher);
+            }
+        },splashDuration);
     }
 }
